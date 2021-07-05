074e194559bd845ed04892642ceb616cc37f742f
Swapna-Gondhi <70559453+Swapna-Gondhi@users.noreply.github.com>
2021-07-05 20:29:04 +0530

    adding about us pages1

---
+++ global-operations-map.php
@@ -0,0 +1,35 @@
+<?php include_once( 'header.php'); ?>
+		<!-- Page Title
+		============================================= -->
+		<section id="page-title" class="page-title-parallax page-title-dark" style="background-image: url(img/banners/Investors-Banner-Img.jpg);">
+			<div class="container clearfix">
+				<h1>DELIVERING ROBUST FINANCIALS</h1> 
+				<h6>We are diligently and sustainably achieving <br>excellence in all that we do</h6>
+				<ol class="breadcrumb">
+					<li class="breadcrumb-item"><a href="#">Home</a></li>
+					<li class="breadcrumb-item"><a href="#">About Us</a></li> 
+					<li class="breadcrumb-item"><a href="#">At a Glance</a></li>
+					<li class="breadcrumb-item active" aria-current="page">Global Operations Map</li>
+				</ol> 
+
+			</div>  
+		</section><!-- #page-title end --> 
+		<!-- Content
+		============================================= -->
+		<section id="content">
+			<div class="content-wrap">
+				<div class="container">
+
+					<div class="row col-mb-50">
+						<div class="col-12">
+							<div class="head-title">
+								<h2>Global Operations Map</h2>
+								 	 	 	  
+							</div> 
+						</div>  
+					</div> 
+				</div>
+			</div>
+		</section><!-- #content end -->
+
+		<?php  include_once( 'footer.php'); ?>
